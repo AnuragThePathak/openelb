eadc1e5e11a603f82640a1ce26ac88f4397c8d1e
Anurag Pathak <anuragpathak911@gmail.com>
2022-10-02 23:32:09 +0530

    update

--- pkg/server/internal/lib/server.go
+++ pkg/server/internal/lib/server.go
@@ -25,8 +25,8 @@ func NewHTTPServer(routers []Router, options options.Options) *server {
 
 	return &server{
 		handler: cors.New(cors.Options{
-			AllowedOrigins: []string{"*"},
-			AllowCredentials: false,
+			AllowedOrigins: []string{"http://localhost:3000"},
+			AllowCredentials: true,
 			AllowedMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
 			AllowedHeaders: []string{"Accept", "Content-Type"},
 		}).Handler(httpRouter),
